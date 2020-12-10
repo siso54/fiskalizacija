83bd60ff6c7a8ac06bdaa7443d587d570afd8a33
Samo Topolsek <samo.topolsek@siel.si>
2020-12-10 13:23:32 +0100

    Changed QR code correction lovel

--- src/QR/QRCode.php
+++ src/QR/QRCode.php
@@ -39,7 +39,7 @@ class QRCode
         $qrCode->setMargin(10);
         $qrCode->setWriterByName('png');
         $qrCode->setEncoding('UTF-8');
-        $qrCode->setErrorCorrectionLevel(ErrorCorrectionLevel::HIGH());
+        $qrCode->setErrorCorrectionLevel(ErrorCorrectionLevel::LOW());
         $qrCode->setForegroundColor(['r' => 0, 'g' => 0, 'b' => 0, 'a' => 0]);
         $qrCode->setBackgroundColor(['r' => 255, 'g' => 255, 'b' => 255, 'a' => 0]);
         $qrCode->setRoundBlockSize(true);
